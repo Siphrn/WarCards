9b9700b8709252e9c694ca8fc7b84f3b954042bc
siphrn <lucaskordzakhia@yahoo.com>
2026-08-15 00:07:21 -0400

    Added Player Class with a playCard() method.
    The method still needs to remove the card, TBA.

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,15 @@
-public class Player {
+import java.util.concurrent.ThreadLocalRandom;
 
+public class Player {
+    public Card[] cards;
+
+    public Player(Card[] cards){
+        this.cards = cards;
+    }
+
+    public Card playCard(){
+        int numOfCards = this.cards.length;
+        int randomNumIndex = ThreadLocalRandom.current().nextInt(0, numOfCards - 1);
+        return this.cards[randomNumIndex];
+    }
 }
